ebabe0b5eed92bcd51e880c033c6de6aa16bcdd8
Roman Yatsynych <rom.yatsynych@gmail.com>
2025-03-31 23:34:22 +0300

    oops..

--- services/deleteSound.ts
+++ services/deleteSound.ts
@@ -10,17 +10,25 @@ const deleteSound = async (interaction: ButtonInteraction) => {
   await (await interaction.channel.messages.fetch(messageId)).delete();
   const map = JSON.parse(fs.readFileSync(path.resolve('sounds/map.json'), 'utf-8'));
   delete map[soundName as string];
-  const buttons = new ActionRowBuilder<ButtonBuilder>();
-  for (const sound of Object.keys(map)) {
-    const button = createButton(sound, map[sound].emoji);
-    buttons.addComponents(button);
+
+  const rows: ActionRowBuilder<ButtonBuilder>[] = [];
+  for (let i = 0; i < Math.ceil(Object.keys(map).length / 5); i++) {
+    const buttons = new ActionRowBuilder<ButtonBuilder>();
+    const sounds = Object.keys(map);
+    for (let j = 0; j < 5; j++) {
+      const sound = sounds[j + i * 5];
+      if (!sound) continue;
+      const button = createButton(sound, map[sound].emoji);
+      buttons.addComponents(button);
+    }
+    rows.push(buttons);
   }
 
   const soundPadMessageId = await global.getSoundPadMessageId();
   console.log(soundPadMessageId);
   // @ts-ignore
   await (await interaction.channel.messages.fetch(soundPadMessageId)).delete()
-  await interaction.followUp({ content: 'Sound Pad' , components: [buttons] });
+  await interaction.followUp({ content: 'Sound Pad' , components: rows });
 
   fs.writeFileSync(path.resolve('sounds/map.json'), JSON.stringify(map));
 }
