4fddb78b8a5f63e5ad9bbda02e4818a1a611910f
Roman Yatsynych <rom.yatsynych@gmail.com>
2025-03-31 23:32:56 +0300

    forgot the delete method :)

--- commands/delete.ts
+++ commands/delete.ts
@@ -11,13 +11,19 @@ export = {
     const map = JSON.parse(fs.readFileSync(path.resolve('sounds/map.json'), 'utf-8'));
     const message = await interaction.fetchReply();
 
-    const buttons = new ActionRowBuilder<ButtonBuilder>();
-    for (const sound of Object.keys(map)) {
-
-      const button = createButton(`delete@${sound}@${message.id}`, map[sound].emoji, true);
-      buttons.addComponents(button);
+    const rows: ActionRowBuilder<ButtonBuilder>[] = [];
+    for (let i = 0; i < Math.ceil(Object.keys(map).length / 5); i++) {
+      const buttons = new ActionRowBuilder<ButtonBuilder>();
+      const sounds = Object.keys(map);
+      for (let j = 0; j < 5; j++) {
+        const sound = sounds[j + i * 5];
+        if (!sound) continue;
+        const button = createButton(`delete@${sound}@${message.id}`, map[sound].emoji, true);
+        buttons.addComponents(button);
+      }
+      rows.push(buttons);
     }
 
-    await interaction.followUp({ content: 'Обери звук який хочеш видалити', components: [buttons] });
+    await interaction.followUp({ content: 'Обери звук який хочеш видалити', components: rows });
   }
 }
